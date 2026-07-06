20ebbf12fe5269582e078894a166ccdb344b1801
fabioferreolicap53 <fabioferreoli.cap53@gmail.com>
2026-07-06 06:53:50 -0300

    remove test-regex.js from git tracking

--- test-regex.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const c = fs.readFileSync('node_modules/react-dom/cjs/react-dom-client.development.js', 'utf8');
-const RE = /console\.info\(\s*"%cDownload the React DevTools[\s\S]*?"font-weight:bold"\s*\)\s*;/g;
-const m = c.match(RE);
-console.log('Match:', m ? m.length : 0);
-if (m) console.log(m[0].substring(0, 200));
