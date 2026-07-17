2096944de0e1c27ac3ab29dcf90046c4bf56afb9
fabioferreolicap53 <fabioferreoli.cap53@gmail.com>
2026-07-17 12:35:11 -0300

    232

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -2,7 +2,7 @@
 import PocketBase from 'pocketbase';
 
 // Substitua pela URL da sua VM na Oracle Cloud (Use HTTPS se disponível)
-const pocketbaseUrl = import.meta.env.VITE_POCKETBASE_URL || 'https://centraldedados.book154.com.br';
+const pocketbaseUrl = import.meta.env.VITE_POCKETBASE_URL || 'https://centraldedados.dev.br';
 export const pb = new PocketBase(pocketbaseUrl);
 
 // Desativa o auto cancelamento de requisições duplicadas (opcional, mas recomendado para React)
